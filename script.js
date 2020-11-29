a3cd1c43fdf35ae4469850600cc5062b8c30abf8
sean0x0 <75191538+sean0x0@users.noreply.github.com>
2020-11-29 11:33:17 +0800

    small changes to all the files

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ const minusButton = document.querySelector("#minus");
 const resetButton = document.querySelector("#reset");
 
 // 按下 +1 的按鈕事件
-addButton.addEventListener("click", function() {
+addButton.addEventListener("click", () => {
     count = count + 2;
     value.textContent = count;
 });
 
 // 按下 -1 的按鈕事件
-minusButton.addEventListener("click", function() {
+minusButton.addEventListener("click", () => {
     count = count - 2;
     value.textContent = count;
 });
 
 // 按下 歸零 的按鈕事件
-resetButton.addEventListener("click", function() {
+resetButton.addEventListener("click", () => {
     count = 0;
     value.textContent = count;
 });
